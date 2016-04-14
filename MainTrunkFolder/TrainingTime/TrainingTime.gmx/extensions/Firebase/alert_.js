02995c2eb5bcc7b90aaf80ef1fd9fd349ba1a040
huma-dev <huma.dev04@gmail.com>
2016-04-14 15:47:17 +0500

    Login room added. Login functionality added. Checking of login credentials from Firebase is undone.

--- MainTrunkFolder/TrainingTime/TrainingTime.gmx/extensions/Firebase/alert_.js
+++ MainTrunkFolder/TrainingTime/TrainingTime.gmx/extensions/Firebase/alert_.js
@@ -1,2 +1,69 @@
-alert_fn()
-{ alert("Hello World") ;}
+function alert_fn()
+{
+
+
+alert("Hello World");
+
+}
+function installFirebase()
+{/*
+	var head= document.getElementsByTagName('head')[0];
+   var script= document.createElement('script');
+   script.type= 'text/javascript';
+   script.src= 'https://cdn.firebase.com/js/client/2.4.1/firebase.js';
+   head.appendChild(script);*/
+}
+
+function  createUser(varUser, varPass)
+{
+	var myFirebaseRef = new Firebase("https://gmdemoapp.firebaseio.com/user");
+	myFirebaseRef.push({
+  
+  loginName: varUser,
+  password: varPass,
+  score: {
+    level1: "undefined",
+    level2: "undefined",
+    level3: "undefined"
+  }
+});
+}
+
+function setUserInfo(){
+	var myFirebaseRef = new Firebase("https://gmdemoapp.firebaseio.com");
+	myFirebaseRef.set({
+  title: "Hello World!",
+  author: "Firebase",
+  location: {
+    city: "San Francisco",
+    state: "California",
+    zip: 94103
+  }
+});
+}
+	
+function getUserInfo(user){
+	/*
+	var myFirebaseRef = new Firebase("https://gmdemoapp.firebaseio.com");
+	myFirebaseRef.child("location/city").on("value", function(snapshot) {
+  alert(snapshot.val());  // Alerts "San Francisco"
+});
+*/
+var data = null;
+var key = null;
+var ref = new Firebase("https://gmdemoapp.firebaseio.com/user");
+ref.orderByChild("loginName").equalTo(user).on("child_added", function(snapshot) {
+  console.log( snapshot.child("score/level1").val());
+  
+});
+}
+function getTotalQuestions(level){
+	
+	var myFirebaseRef = new Firebase("https://gmdemoapp.firebaseio.com/questions/"+level+"");
+	myFirebaseRef.once("value", function(snapshot) {
+	var a = snapshot.numChildren()
+	 console.log("num of questions of the user "+level+"= "+a);
+	});
+ 
+
+}
